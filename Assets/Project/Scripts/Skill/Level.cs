ed1e4e00fd71931dc28d1df4ac3104cfeecd8067
Arseniy-1 <92578166+Arseniy-1@users.noreply.github.com>
2025-01-28 14:41:40 +0300

    1

--- Assets/Project/Scripts/Skill/Level.cs
+++ Assets/Project/Scripts/Skill/Level.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
     [SerializeField] private ExperienceStorage _experienceStorage;
+    [SerializeField] private LevelConfig _levelConfig;
+    
     private int _currentLevel;
 
     public event Action LevelRaised;
---
+++ Assets/Project/Scripts/Skill/LevelConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class LevelConfig
+{
+    [SerializeField] private List<float> _multipliers;
+    
+    public IReadOnlyList<float> Multipliers => _multipliers;
+
+    private int MaxSize = 5;
+    
+    private void OnValidate()
+    {
+        if (_multipliers.Count > MaxSize)
+        {
+            _multipliers = _multipliers.GetRange(0, MaxSize);
+        }
+    }
+}
