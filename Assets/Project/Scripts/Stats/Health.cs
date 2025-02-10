27cded8808fc715901fa3b9a5cc06566f82e9f11
sozooo <110661290+sozooo@users.noreply.github.com>
2025-02-10 05:29:45 +0300

    Health Component Realization

--- Assets/Project/Scripts/Stats/Health.cs
+++ Assets/Project/Scripts/Stats/Health.cs
@@ -1,77 +1,62 @@
 using System;
+using UnityEngine;
 
-[System.Serializable]
+[Serializable]
 public class Health : BaseStat
 {
     public event Action LostHealth;
 
+    private float MaxHealth => CalculateValue();
+
     public void Heal(int amount)
     {
-        CurrentValue += amount;
+        if(amount < 0)
+            throw new ArgumentOutOfRangeException(amount.ToString());
+        
+        CurrentValue = Mathf.Clamp(CurrentValue + amount, 0, MaxHealth);
     }
 
     public void TakeDamage(int amount)
     {
-
+        if(amount < 0)
+            throw new ArgumentOutOfRangeException(amount.ToString());
+        
+        CurrentValue = Mathf.Clamp(CurrentValue - amount, 0, MaxHealth);
+        
+        if(CurrentValue <= 0)
+            LostHealth?.Invoke();
     }
-
-    public void SetHealth(int amount)
-    {
-
-    }
 }
 
-[System.Serializable]
-public class JumpDistance : BaseStat
-{
-}
+[Serializable]
+public class JumpDistance : BaseStat { }
 
-[System.Serializable]
-public class JumpTime : BaseStat
-{
-}
+[Serializable]
+public class JumpTime : BaseStat { }
 
-[System.Serializable]
-public class JumpReloadTime : BaseStat
-{
-}
+[Serializable]
+public class JumpReloadTime : BaseStat { }
 
-[System.Serializable]
-public class WeaponSpread : BaseStat
-{
-}
+[Serializable]
+public class WeaponSpread : BaseStat { }
 
-[System.Serializable]
-public class WeaponDamage : BaseStat
-{
-}
+[Serializable]
+public class WeaponDamage : BaseStat { }
 
-[System.Serializable]
-public class BulletPerShootCount : BaseStat
-{
-}
+[Serializable]
+public class BulletPerShootCount : BaseStat { }
 
-[System.Serializable]
-public class WeaponBulletReloadTime : BaseStat
-{
-}
+[Serializable]
+public class WeaponBulletReloadTime : BaseStat { }
 
-[System.Serializable]
-public class WeaponRechargingTime : BaseStat
-{
-}
+[Serializable]
+public class WeaponRechargingTime : BaseStat { }
 
-[System.Serializable]
-public class WeaponMagazineSize : BaseStat
-{
-}
+[Serializable]
+public class WeaponMagazineSize : BaseStat { }
 
-[System.Serializable]
-public class MagnetRange : BaseStat
-{
-}
+[Serializable]
+public class MagnetRange : BaseStat { }
 
-[System.Serializable]
-public class MagnetForce : BaseStat
-{
-}
+[Serializable]
+public class MagnetForce : BaseStat { }
