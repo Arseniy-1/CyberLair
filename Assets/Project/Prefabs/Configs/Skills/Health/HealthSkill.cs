48cea1a907a7ecb411a8d6a97f0f8ce3232d61de
sozooo <110661290+sozooo@users.noreply.github.com>
2025-02-11 20:33:27 +0300

    Health + Regen Connect

--- Assets/Project/Prefabs/Configs/Skills/Health/HealthSkill.cs
+++ Assets/Project/Prefabs/Configs/Skills/Health/HealthSkill.cs
@@ -4,9 +4,11 @@
 public class HealthSkill : Skill
 {
     [SerializeField] private StatModifier _healthModifier;
+    [SerializeField] private StatModifier _regenerationModifier;
     
     public override void Apply(SkillData skillData)
     {
-        skillData.PlayerStats.Health.AddModifier(_healthModifier);
+        skillData.PlayerStats.Health.AddModifier(_healthModifier.Copy());
+        skillData.PlayerStats.Health.RegenerateAmount.AddModifier(_regenerationModifier.Copy());
     }
 }
