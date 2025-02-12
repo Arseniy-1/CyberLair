2af3213a3102d29b915a8c1dfeebe212bcd787f6
sozooo <110661290+sozooo@users.noreply.github.com>
2025-02-12 04:53:21 +0300

    TacticalEfficiencySkill

---
+++ Assets/Project/Prefabs/Configs/Skills/TacticalEfficiency/TacticalEfficiencySkill.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Project.Prefabs.Configs.Skills.TacticalEfficiency
+{
+    [CreateAssetMenu(fileName = "TacticalEfficiencySkill", menuName = "Skill/Simple/TacticalEfficiency", order = 51)]
+    public class TacticalEfficiencySkill : Skill
+    {
+        [SerializeField] private StatModifier _healthModifier;
+        [SerializeField] private StatModifier _damageModifier;
+        
+        public override void Apply(SkillData skillData)
+        {
+            skillData.PlayerStats.Health.AddModifier(_healthModifier);
+            skillData.PlayerStats.WeaponDamage.AddModifier(_damageModifier);
+            
+        }
+    }
+}
