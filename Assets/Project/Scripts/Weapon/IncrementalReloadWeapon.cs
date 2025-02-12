c90d0e5b7d5ed7526df2c1e4beafebbaa36e6792
sozooo <110661290+sozooo@users.noreply.github.com>
2025-02-12 03:53:39 +0300

    FormerlySerializedAs Removed

--- Assets/Project/Scripts/Weapon/IncrementalReloadWeapon.cs
+++ Assets/Project/Scripts/Weapon/IncrementalReloadWeapon.cs
@@ -6,7 +6,7 @@ using UnityEngine.Serialization;
 
 public class IncrementalReloadWeapon : Weapon
 {
-    [FormerlySerializedAs("_currentMagazineSize")] [SerializeField] private int currentCurrentMagazineSize;
+    [SerializeField] private int currentCurrentMagazineSize;
 
     private Coroutine _reloadCoroutine;
 
