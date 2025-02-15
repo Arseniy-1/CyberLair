8a7c7d3302624af7a3b0fc4567ccf08d056e2eaf
sozooo <110661290+sozooo@users.noreply.github.com>
2025-02-15 20:23:32 +0300

    JumpSwirlSkill

---
+++ Assets/Project/Prefabs/Configs/Skills/JumpSwirl/JumpSwirlSkill.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Project.Prefabs.Configs.Skills.JumpSwirl
+{
+    [CreateAssetMenu(fileName = "JumpSwirlSkill", menuName = "Skill/Hard/JumpSwirl", order = 51)]
+    public class JumpSwirlSkill : HardSkill
+    {
+        [SerializeField] private StatModifier _jumpDistanceModifier;
+        [SerializeField] private StatModifier _magnetRangeModifier;
+        
+        public override void Apply(SkillData skillData)
+        {
+            skillData.PlayerStats.JumpDistance.AddModifier(_jumpDistanceModifier);
+            skillData.PlayerStats.MagnetRange.AddModifier(_magnetRangeModifier);
+        }
+    }
+}
