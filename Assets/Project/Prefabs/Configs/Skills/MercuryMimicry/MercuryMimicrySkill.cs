ea95b3b8785c72125cee853b58b854b153c0d873
sozooo <110661290+sozooo@users.noreply.github.com>
2025-02-15 20:27:03 +0300

    MercuryMimicrySkill + SO

---
+++ Assets/Project/Prefabs/Configs/Skills/MercuryMimicry/MercuryMimicrySkill.cs
@@ -0,0 +1,16 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Project.Prefabs.Configs.Skills.MercuryMimicry
+{
+    [CreateAssetMenu(fileName = "MercuryMimicrySkill", menuName = "Skill/Hard/MercuryMimicry", order = 51)]
+    public class MercuryMimicrySkill : HardSkill
+    {
+        [SerializeField] private MercuryMimicry _mercuryMimicry;
+        
+        public override void Apply(SkillData skillData)
+        {
+            _mercuryMimicry.Initialize(skillData);
+        }
+    }
+}
