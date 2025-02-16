64bf4a9e0ff4f7d1ba8ff3c7d174219c195c2cea
sozooo <110661290+sozooo@users.noreply.github.com>
2025-02-16 09:35:56 +0300

    PhantomArrowSkill Release

--- Assets/Project/Prefabs/Configs/Skills/MagicArrow/MagicArrowSkill.cs
+++ Assets/Project/Prefabs/Configs/Skills/MagicArrow/MagicArrowSkill.cs
@@ -6,12 +6,13 @@ namespace Project.Scripts.LevelSystem.ActiveSkills
     [CreateAssetMenu(fileName = "MagicArrowSkill", menuName = "Skill/Simple/MagicArrow", order = 51)]
     public class MagicArrowSkill : Skill
     {
-        [SerializeField] private MagicArrowSpawner _magicArrowSpawner;
         [SerializeField] private MagicArrow _magicArrowPrefab;
+        
+        [field:SerializeField] public MagicArrowSpawner MagicArrowSpawner { get; private set; }
         
         public override void Apply(SkillData skillData)
         {
-            _magicArrowSpawner.Initialize(_magicArrowPrefab, skillData.WeaponHolder.transform);
+            MagicArrowSpawner.Initialize(_magicArrowPrefab, skillData.WeaponHolder.transform);
         }
     }
 }
