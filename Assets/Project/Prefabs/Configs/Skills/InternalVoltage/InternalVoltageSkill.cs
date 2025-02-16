6c7943ece8abcec34329c0f30fb361275c12c8e5
sozooo <110661290+sozooo@users.noreply.github.com>
2025-02-16 10:13:15 +0300

    InternalVoltageSkill CreateAssetMenu Correction

--- Assets/Project/Prefabs/Configs/Skills/InternalVoltage/InternalVoltageSkill.cs
+++ Assets/Project/Prefabs/Configs/Skills/InternalVoltage/InternalVoltageSkill.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace Project.Prefabs.Configs.Skills.InternalVoltage
 {
+    [CreateAssetMenu(fileName = "InternalVoltageSkill", menuName = "Skill/Hard/InternalVoltage", order = 51)]
     public class InternalVoltageSkill : HardSkill
     {
         [SerializeField] private InternalVoltage _internalVoltage;
