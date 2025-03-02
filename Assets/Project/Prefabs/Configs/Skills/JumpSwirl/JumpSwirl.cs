9cebac6f3d135faadc0542d6b1b3072925705d89
Arseniy-1 <92578166+Arseniy-1@users.noreply.github.com>
2025-03-02 12:50:01 +0300

    RemakeJumpSwirl+TestThis

--- Assets/Project/Prefabs/Configs/Skills/JumpSwirl/JumpSwirl.cs
+++ Assets/Project/Prefabs/Configs/Skills/JumpSwirl/JumpSwirl.cs
@@ -1,29 +1,40 @@
-using Project.Prefabs.Configs.Skills.Durability;
+using UnityEngine;
 
 namespace Project.Prefabs.Configs.Skills.JumpSwirl
 {
     public class JumpSwirl : ISkillInstance
     {
-        private StatModifier _jumpDistanceModifier;
-        private StatModifier _magnetRangeModifier;
-        
         private SkillData _skillData;
-        
+        private JumpSwirlSkill _skill;
+
         public JumpSwirl(SkillData skillData, JumpSwirlSkill skill)
         {
-            _jumpDistanceModifier = skill.JumpDistanceModifier.Copy();
-            _magnetRangeModifier = skill.MagnetRangeModifier.Copy();
-            
             _skillData = skillData;
-            
-            skillData.PlayerStats.JumpDistance.AddModifier(_jumpDistanceModifier);
-            skillData.PlayerStats.MagnetRange.AddModifier(_magnetRangeModifier);
+            _skill = skill;
+
+            _skillData.PlayerJumper.JumpPerformed += HandleJump;
         }
 
         public void Disable()
         {
-            _skillData.PlayerStats.JumpDistance.RemoveModifier(_jumpDistanceModifier);
-            _skillData.PlayerStats.MagnetRange.RemoveModifier(_magnetRangeModifier);
+            _skillData.PlayerJumper.JumpPerformed -= HandleJump;
+        }
+
+        private void HandleJump()
+        {
+            var playerPosition = _skillData.PlayerJumper.transform.position;
+            var enemies = Physics2D.OverlapCircleAll(playerPosition, _skill.KnockbackRadius, _skill.EnemyLayer);
+
+            foreach (var enemy in enemies)
+            {
+                if (enemy.TryGetComponent(out IStunable stunable))
+                {
+                    stunable.TakeStun(_skill.StunTime);
+                 
+                    Vector2 knockbackDirection = (enemy.transform.position - playerPosition).normalized;
+                    stunable.Rigidbody2D.AddForce(knockbackDirection * _skill.KnockbackForce, ForceMode2D.Impulse);
+                }
+            }
         }
     }
 }
--- Assets/Project/Prefabs/Configs/Skills/JumpSwirl/JumpSwirlSkill.cs
+++ Assets/Project/Prefabs/Configs/Skills/JumpSwirl/JumpSwirlSkill.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Project.Prefabs.Configs.Skills.JumpSwirl
@@ -5,7 +6,9 @@ namespace Project.Prefabs.Configs.Skills.JumpSwirl
     [CreateAssetMenu(fileName = "JumpSwirlSkill", menuName = "Skill/Hard/JumpSwirl", order = 51)]
     public class JumpSwirlSkill : HardSkill
     {
-        [field: SerializeField] public StatModifier JumpDistanceModifier {get; private set;}
-        [field: SerializeField] public StatModifier MagnetRangeModifier {get; private set;}
+        [field: SerializeField] public float KnockbackForce { get; private set; }
+        [field: SerializeField] public float KnockbackRadius { get; private set; }
+        [field: SerializeField] public float StunTime { get; private set; }
+        [field: SerializeField] public LayerMask EnemyLayer { get; private set; }
     }
 }
