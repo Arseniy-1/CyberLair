6ee3520ccc396e2a1274e7580838f502bdb80686
sozooo <110661290+sozooo@users.noreply.github.com>
2025-03-30 08:37:29 +0300

    Camera Shaker w/ MessageBroker

--- Assets/Project/Scripts/MessageBroker/MessageBrokerHolder.cs
+++ Assets/Project/Scripts/MessageBroker/MessageBrokerHolder.cs
@@ -3,4 +3,5 @@ using UniRx;
 public static class MessageBrokerHolder
 {
     public static IMessageBroker Enemy { get; private set; } = new MessageBroker();
+    public static IMessageBroker Camera { get; private set; } = new MessageBroker();
 }
--- Assets/Project/Scripts/Services/CameraShaker.cs
+++ Assets/Project/Scripts/Services/CameraShaker.cs
@@ -1,4 +1,7 @@
 using DG.Tweening;
+using Project.Scripts.MessageBroker.CameraMessageBrokers;
+using UniRx;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace DefaultNamespace.Tween
@@ -6,14 +9,34 @@ namespace DefaultNamespace.Tween
     public class CameraShaker : MonoBehaviour
     {
         [SerializeField] private Camera _camera;
+
+        [Header("Shake Settings")]
+        [SerializeField] private float _duration;
+        [SerializeField] private float _strength;
+        [SerializeField] private int _vibrato;
+        [SerializeField] private float _randomness;
         
         private Vector3 _originalPosition;
+        private CompositeDisposable _disposable;
 
-        public void Shake(float duration, float strength, int vibrato, float randomness)
+        private void OnEnable()
+        {
+            _disposable = new CompositeDisposable();
+            
+            MessageBrokerHolder.Enemy.Receive<CameraShakeMessage>().Subscribe(_ => Shake())
+                .AddTo(_disposable);
+        }
+
+        private void OnDisable()
+        {
+            _disposable.Dispose();
+        }
+
+        private void Shake()
         {
             _originalPosition = _camera.transform.localPosition;
             
-            _camera.transform.DOShakePosition(duration, strength, vibrato, randomness)
+            _camera.transform.DOShakePosition(_duration, _strength, _vibrato, _randomness)
                 .OnComplete(() => _camera.transform.localPosition = _originalPosition);
         }
     }
