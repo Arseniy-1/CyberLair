0a3c4e9f423f6928c68f8ce3303bbb87deaeb529
sozooo <110661290+sozooo@users.noreply.github.com>
2025-04-08 18:29:11 +0300

    BigSkillView

--- Assets/Project/Prefabs/Configs/Skills/Boomerang/BoomerangSkill.cs
+++ Assets/Project/Prefabs/Configs/Skills/Boomerang/BoomerangSkill.cs
@@ -1,4 +1,7 @@
+using UnityEngine;
+
 namespace Project.Prefabs.Configs.Skills.Boomerang
 {
+    [CreateAssetMenu(fileName = "BoomerangSkill", menuName = "Skill/Simple/Boomerang", order = 51)]
     public class BoomerangSkill : OrbitalSkill { }
 }
--- Assets/Project/Prefabs/Configs/Skills/Lair-1/LairOneSkill.cs
+++ Assets/Project/Prefabs/Configs/Skills/Lair-1/LairOneSkill.cs
@@ -1,4 +1,7 @@
+using UnityEngine;
+
 namespace Project.Prefabs.Configs.Skills.Lair_1
 {
+    [CreateAssetMenu(fileName = "LairOneSkill", menuName = "Skill/Simple/LairOne", order = 51)]
     public class LairOneSkill : OrbitalSkill { }
 }
