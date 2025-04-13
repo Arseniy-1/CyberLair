5b1d78e5c7ee2195b26331307e2f3b336e40e3b4
Arseniy-1 <92578166+Arseniy-1@users.noreply.github.com>
2025-04-13 16:21:20 +0300

    1

---
+++ Assets/Project/Scripts/PlayerSystem/MobileShootZone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MobileShootZone : MonoBehaviour, IPointerDownHandler
+{
+    public event Action OnShootButtonPressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+        Debug.Log("OnPointerDown");
+        OnShootButtonPressed?.Invoke();
+    }
+}
