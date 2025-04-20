aacfd75d71ad3e8f588350b4fb7b5d0bb770a4c6
Arseniy-1 <92578166+Arseniy-1@users.noreply.github.com>
2025-04-20 13:41:10 +0300

    AddLeaderBoard

---
+++ Assets/Project/Sounds/ExtendedVolumeChanger.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class ExtendedVolumeChanger : MonoBehaviour
+{
+    [SerializeField] private Toggle _button;
+    [SerializeField] private AudioMixerGroup _audioMixer;
+    [SerializeField] private Slider _slider;
+
+    private bool _isEnabled = true;
+    private float _minlVolume = -80;
+
+    private void OnEnable()
+    {
+        _button.onValueChanged.AddListener(ToggleMusic);
+        _slider.onValueChanged.AddListener(Change);
+    }
+
+    private void OnDisable()
+    {
+        _button.onValueChanged.RemoveListener(ToggleMusic);
+        _slider.onValueChanged.RemoveListener(Change);
+    }
+
+    private void ToggleMusic(bool isMuted)
+    {
+        if (isMuted == false)
+            _audioMixer.audioMixer.SetFloat(_audioMixer.name, _minlVolume);
+        else
+            SetCurrentVolume(_slider.value);
+
+        _isEnabled = !isMuted;
+    }
+
+    private void Change(float amount)
+    {
+        if (_isEnabled)
+            SetCurrentVolume(amount);
+    }
+
+    private void SetCurrentVolume(float volume)
+    {
+        _audioMixer.audioMixer.SetFloat(_audioMixer.name, Mathf.Log10(volume) * 40);
+    }
+}
---
+++ Assets/Project/Sounds/VolumeChanger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeChanger : MonoBehaviour
+{
+    [SerializeField] private AudioMixerGroup _audioMixer;
+    [SerializeField] private Slider _slider;
+
+    private void OnEnable()
+    {
+        _slider.onValueChanged.AddListener(Change);
+    }
+
+    private void OnDisable()
+    {
+        _slider.onValueChanged.RemoveListener(Change);
+    }
+
+    private void Change(float value)
+    {
+        _audioMixer.audioMixer.SetFloat(_audioMixer.name, Mathf.Log10(_slider.value) * 40);
+    }
+}
