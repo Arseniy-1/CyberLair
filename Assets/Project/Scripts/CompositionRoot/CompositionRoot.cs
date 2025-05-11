63ee1f07aed2f7caa88ba9336b12858bc0f9dfe4
Arseniy-1 <92578166+Arseniy-1@users.noreply.github.com>
2025-05-12 00:06:09 +0300

    1

--- Assets/Project/Scripts/CompositionRoot/CompositionRoot.cs
+++ Assets/Project/Scripts/CompositionRoot/CompositionRoot.cs
@@ -65,7 +65,7 @@ namespace Project.Scripts.CompositionRoot
             _arena.WavesDone += ShowWinScreen;
             YandexGame.RewardVideoEvent += OnRewarded;
 
-            if (true)
+            if (YandexGame.savesData.isFirstSession)
             {
                 _tutorialView.gameObject.SetActive(true);
                 _tutorialView.OnFinished += OnTutorialFinished;
